9329dbe85bfc42c845f0f382779559094a0201e6
serosero <sggblank@gmail.com>
2025-05-04 23:37:54 +0800

    🧢 Abstract Base Class initial

--- main.py
+++ main.py
@@ -2,5 +2,24 @@ from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
 
-    def __init__(self):
+    def __init__(self, speed, passenger_limit, fuel_type):
+        self.speed = speed
+        self.passenger_limit = passenger_limit
+        self.fuel_type = fuel_type
+
+    @property
+    @abstractmethod
+    def classification(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    @abstractmethod
+    def drive(self):
         pass
