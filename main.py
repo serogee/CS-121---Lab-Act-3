17a6ea66e8efffafdfed4b0542a63a7f616c193e
serosero <sggblank@gmail.com>
2025-05-05 13:28:53 +0800

    🚗 Vehicle Last Ver

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+
+class Classification(Enum):
+
+    CAR = "car"
 
 class Vehicle(ABC):
 
-    def __init__(self, speed, passenger_limit, fuel_type):
-        self.speed = speed
+    def __init__(self, passenger_limit, fuel_type):
         self.passenger_limit = passenger_limit
         self.fuel_type = fuel_type
+
+        self.__speed = 0
+        self.__is_running = False
+
+    @property
+    def speed(self):
+        return self.__speed
+        
+    @property
+    def is_running(self):
+        return self.__is_running
 
     @property
     @abstractmethod
     def classification(self):
-        pass
+        ...
+        
+    def accelerate(self):
+        ...
 
-    @abstractmethod
+    def decelerate(self):
+        ...
+
     def start(self):
-        pass
+        ...
 
-    @abstractmethod
     def stop(self):
-        pass
-
-    @abstractmethod
-    def drive(self):
-        pass
+        ...
